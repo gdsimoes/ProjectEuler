7baf1fcd48d5d674014cff68cc63a281d013bdd3
Guilherme Dias Simões <gds.simoes@gmail.com>
2020-08-31 17:19:37 -0400

    Added solution to problem 9

---
+++ 009.js
@@ -0,0 +1,19 @@
+// value of a + b + c
+const sum = 1000;
+
+// since a < b < c 1000/3 is an upper bound for a
+const supA = sum / 3;
+
+// I decided to use a label just to try it
+label: for (let a = 1; a < supA; a++) {
+    // since c = sum - (a + b) and b < c we also get an upper bound for b
+    const supB = (sum - a) / 2;
+
+    for (let b = a + 1; b < supB; b++) {
+        const c = sum - (a + b);
+        if (a ** 2 + b ** 2 === c ** 2) {
+            console.log(a * b * c);
+            break label;
+        }
+    }
+}
