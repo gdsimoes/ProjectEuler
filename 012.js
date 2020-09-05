08ca62d3a87a5347373f3002929713510c1072dc
Guilherme Dias Simões <gds.simoes@gmail.com>
2020-09-05 00:06:58 -0400

    Add solution to problem 12

---
+++ 012.js
@@ -0,0 +1,12 @@
+let misc = require("./misc");
+
+// return the nth triangle number
+function nthTriangleNumber(n) {
+    return (n * (n + 1)) / 2;
+}
+
+// return the first triangle number to have over five hundred divisors
+let n;
+for (n = 1; misc.allFactors(nthTriangleNumber(n)).length <= 500; n++);
+
+console.log(nthTriangleNumber(n));
