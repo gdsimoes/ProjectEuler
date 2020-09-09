f0cacc995465bb3273775df0be3ae285fbe003fd
Guilherme Dias Simões <gds.simoes@gmail.com>
2020-09-08 21:16:46 -0400

    Add solution to problem 14

---
+++ 014.js
@@ -0,0 +1,31 @@
+function collatz(n) {
+    if (n % 2 == 0) {
+        return n / 2;
+    } else {
+        return 3 * n + 1;
+    }
+}
+
+function lengthOfCollatz(n) {
+    let length = 1;
+
+    while (n !== 1) {
+        n = collatz(n);
+        length++;
+    }
+
+    return length;
+}
+
+// Find which starting number, under one million, produces the longest chain.
+let maxLength = 1;
+let result = 1;
+for (let i = 1; i < 1_000_000; i++) {
+    const length = lengthOfCollatz(i);
+    if (length > maxLength) {
+        maxLength = length;
+        result = i;
+    }
+}
+
+console.log(result);
