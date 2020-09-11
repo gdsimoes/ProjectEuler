194467f3acce8223a06bbeedc37ea349621a678f
Guilherme Dias Simões <gds.simoes@gmail.com>
2020-09-10 22:57:29 -0400

    Add solution to problem 16

---
+++ 016.js
@@ -0,0 +1,13 @@
+// I dislike problems that depend on the decimal representation of numbers
+// because I consider them artificial. So I decided to use BigInts and make
+// this problem trivial
+
+let bigNumber = 2n ** 1000n;
+
+let result = 0n;
+while (bigNumber !== 0n) {
+    result += bigNumber % 10n;
+    bigNumber /= 10n;
+}
+
+console.log(result);
