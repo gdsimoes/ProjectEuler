2eb1083b8a957d4d86c3b063d3750336f5a71bc6
Guilherme Dias Simões <gds.simoes@gmail.com>
2020-09-21 17:50:57 -0400

    Add solution to problem 21

---
+++ 021.js
@@ -0,0 +1,33 @@
+// This is another one of these problems that seems highly artificial to me. Who cares about amicable numbers?
+
+let misc = require("./misc");
+
+// Return true if a and b are amicable and false otherwise. If only a is given return amicable(a, sumA)
+function amicable(a, b) {
+    let properDivisorsOfA = misc.allFactors(a);
+    properDivisorsOfA.pop();
+    const sumA = properDivisorsOfA.reduce((x, y) => x + y, 0);
+
+    // You can't be friends with yourself!
+    if (a === b) {
+        return false;
+        // b exists?
+    } else if (b === undefined) {
+        return amicable(a, sumA);
+    }
+
+    let properDivisorsOfB = misc.allFactors(b);
+    properDivisorsOfB.pop();
+    const sumB = properDivisorsOfB.reduce((x, y) => x + y, 0);
+
+    return sumA === b && sumB === a;
+}
+
+let answer = 0;
+for (let i = 2; i < 10000; i++) {
+    if (amicable(i)) {
+        answer += i;
+    }
+}
+
+console.log(answer);
