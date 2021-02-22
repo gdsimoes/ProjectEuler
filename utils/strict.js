5fae3b1e656f2297f76b06119b06d1283b29c90b
Guilherme Dias Simões <gds.simoes@gmail.com>
2021-02-22 13:53:55 -0500

    Add utils folder and strict script
    This scripts adds the 'use strict' directive to all javascript files in the main directory.

---
+++ utils/strict.js
@@ -0,0 +1,36 @@
+// This small script add "use strict" to all JavaScript files in a given folder
+
+"use strict";
+
+const fs = require("fs");
+
+// List of all JavaScript files in directory
+const fileList = fs.readdirSync("..").filter((x) => x.endsWith(".js"));
+
+for (const filename of fileList) {
+    addStrict(`../${filename}`);
+}
+
+// Add "use strict" to the 'filename' file
+function addStrict(filename) {
+    const lines = fs.readFileSync(filename).toString().split("\n");
+
+    const index = firstLine(lines);
+
+    const directive = '"use strict";';
+    if (lines[index] !== directive) {
+        lines.splice(index, 0, directive + "\n");
+    }
+
+    const contents = lines.join("\n");
+    fs.writeFileSync(filename, contents);
+}
+
+// Return the index of the first line in 'lines' which is not empty or a single-line comment
+function firstLine(lines) {
+    for (const i in lines) {
+        if (lines[i] !== "" && !lines[i].startsWith("//")) {
+            return i;
+        }
+    }
+}
